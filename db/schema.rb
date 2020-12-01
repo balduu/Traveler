1ee2641e4ac4426bf6fc98c0945d970fb13743dc
PhamMinhHieu1801 <phamhieu08928@gmail.com>
2020-12-01 06:53:08 +0000

    edit profile

--- db/schema.rb
+++ db/schema.rb
@@ -51,6 +51,10 @@ ActiveRecord::Schema.define(version: 2020_12_01_021930) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
+    t.integer "age"
+    t.string "hobby"
+    t.string "local"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
